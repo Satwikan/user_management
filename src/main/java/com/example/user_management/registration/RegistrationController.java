53e6ba1003e4da55908d670a2c0f6a05b6f4f386
satwikan-shopflo <111730360+satwikan-shopflo@users.noreply.github.com>
2022-10-07 16:57:37 +0530

    registration working

---
+++ src/main/java/com/example/user_management/registration/RegistrationController.java
@@ -0,0 +1,16 @@
+package com.example.user_management.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+}
---
+++ src/main/java/com/example/user_management/registration/RegistrationService.java
@@ -0,0 +1,28 @@
+package com.example.user_management.registration;
+
+import com.example.user_management.appuser.AppUser;
+import com.example.user_management.appuser.AppUserRole;
+import com.example.user_management.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
+    }
+}
