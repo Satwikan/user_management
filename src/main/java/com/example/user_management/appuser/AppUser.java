161ae621c3ea816874221bd85c3dfca865d4d840
satwikan-shopflo <111730360+satwikan-shopflo@users.noreply.github.com>
2022-11-07 21:12:45 +0530

    email profiler changed

--- src/main/java/com/example/user_management/appuser/AppUser.java
+++ src/main/java/com/example/user_management/appuser/AppUser.java
@@ -1,6 +1,5 @@
 package com.example.user_management.appuser;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
